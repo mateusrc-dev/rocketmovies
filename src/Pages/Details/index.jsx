69e30ad89e379cc956f0fa7893f176074314c901
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-10-08 20:18:21 -0300

    first commit

---
+++ src/Pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import {Container} from './styles.js'
+
+export function Details() {
+  return(
+  <Container>
+    <h1>Olá mundo!</h1>
+    <p>Meu nome é Mateus, eu sou incrível e inteligente demais!</p>
+  </Container>
+  )
+}
---
+++ src/styles/theme.js
@@ -0,0 +1,12 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: "#1C1B1E",
+    BACKGROUND_800: "#3E3B47",
+    BACKGROUND_700: "#0D0C0F",
+    BACKGROUND_600: "rgba(255, 133, 155, 0.05)",
+    WHITE: "#F4EDE8",
+    PINK: "#FF859B",
+    GRAY_100: "#948F99",
+    GRAY_200: "#999591",
+  }
+}
